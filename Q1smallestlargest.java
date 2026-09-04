74b0f1bca63e4cbe371e5246f2d767836abad443
Prajukta <prajuktaha@gmail.com>
2026-09-04 15:39:24 +0530

    Rename Q1 smallest largest file

--- Q1smallestlargest.java
+++ Q1smallestlargest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class practice {
+public class Q1smallestlargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of input : ");
