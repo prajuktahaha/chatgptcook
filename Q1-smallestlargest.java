f7d64b45e8f4b3414ea0ead668da146f53f32e72
Prajukta <prajuktaha@gmail.com>
2026-09-04 15:07:34 +0530

    idetify the smallest and largest number int he series

---
+++ Q1-smallestlargest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of input : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of Numbers : ");
+        int largest = sc.nextInt();
+        int smallest = largest;
+        for(int i = 1 ; i < n ; i++){
+            int numbers = sc.nextInt();
+            if(numbers > largest){
+                largest = numbers;
+            }
+            if(numbers < smallest){
+                smallest = numbers;
+            }
+        }
+        System.out.println("the smallest number is : " + smallest);
+        System.out.println(" the largest number is : " + largest);
+    }
+}
