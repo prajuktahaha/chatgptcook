dad6965dbfa59565ff8678ca02d9927ca4429ebe
Prajukta <prajuktaha@gmail.com>
2026-09-10 01:03:54 +0530

    Update code

---
+++ Q12CounttheNumberofDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q12CounttheNumberofDigits{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        int count = 0;
+        while(n > 0){
+            int digits = n % 10;
+            n = n / 10;
+            count++;
+        }
+        System.out.println("the count of number is " + count);
+    }
+}
