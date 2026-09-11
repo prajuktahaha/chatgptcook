83b835446e76ca80e96f4d11262fb0ea355742fd
Prajukta <prajuktaha@gmail.com>
2026-09-11 06:22:48 +0530

    Update code

---
+++ Q21SumofOddDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Q21SumofOddDigits {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        int odd = 0;
+        int sum = 0;
+        while(n>0){
+            int digit = n % 10;
+            n = n / 10;
+            if(digit % 2 != 0){
+                sum = sum + digit;
+            }
+        }
+        System.out.println("Sum of the odd digits : " + sum);
+    }
+}
