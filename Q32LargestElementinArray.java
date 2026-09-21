ced388248679523191c56beea92268e6818b704d
Prajukta <prajuktaha@gmail.com>
2026-09-21 17:42:20 +0530

    update

--- Q32LargestElementinArray.java
+++ Q32LargestElementinArray.java
@@ -5,7 +5,7 @@ public class Q32LargestElementinArray {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value : ");
         int n = sc.nextInt();
-        int largest = 0;
+        int largest = Integer.MIN_VALUE;
         int [] arr = new int[n];
         System.out.println("Enter the elements : ");
         for(int i = 0 ; i < arr.length ; i++){
